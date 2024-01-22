ce677a8deee3273b73802d145788244d0214eb64
Chris <cqolee@ucdavis.edu>
2024-01-21 17:46:02 -0800

    fixed syntax errors that were found by testing

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -17,12 +17,12 @@ std::string Slice(const std::string &str, ssize_t start, ssize_t end) noexcept{
 }
 
 std::string Capitalize(const std::string &str) noexcept{
-    auto Copy = str;
-    Copy[0] = std::isupper(Copy[0]);
-    for(size_t i = 1; i < Copy.length(); i++) {
-        Copy[i] = tolower(Copy[i]);
+    auto copy = str;
+    copy[0] = toupper(copy[0]);
+    for (size_t Index = 1; Index < copy.length(); Index++) {
+        copy[Index] = tolower(copy[Index]);
     }
-    return Copy;
+    return copy;
 }
 
 std::string Upper(const std::string &str) noexcept{
@@ -45,33 +45,34 @@ std::string Lower(const std::string &str) noexcept{
     return Copy;
 }
 
-std::string LStrip(const std::string &str) noexcept{
-    int i = 0; //the start of string
+std::string LStrip(const std::string &str) noexcept {
+    int i = 0; // the start of string
     while (i < str.length() && std::isspace(str[i])) {
         i++;
     }
     return str.substr(i);
 }
 
-std::string RStrip(const std::string &str) noexcept{
-    int i = str.length(); //the end of string
-    while (i > str.length() && std::isspace(str[i])) {
+std::string RStrip(const std::string &str) noexcept {
+    int i = str.length() - 1; // the end of string
+    while (i >= 0 && std::isspace(str[i])) {
         i--;
     }
-    return str.substr(i);
+    return str.substr(0, i + 1);
 }
 
-std::string Strip(const std::string &str) noexcept{
-    int i = 0; //the start of string
-    int j = str.length(); //the end of string
-    while (i < str.length() && std::isspace(str[i])) {
+std::string Strip(const std::string &str) noexcept {
+    int i = 0;           // the start of string
+    int j = str.length(); // the end of string
+    while (i < j && std::isspace(str[i])) {
         i++;
     }
-    while (j > str.length() && std::isspace(str[j])) {
-        j++;
+    while (j > i && std::isspace(str[j - 1])) {
+        j--;
     }
     return str.substr(i, j - i);
 }
+
 
 std::string Center(const std::string &str, int width, char fill) noexcept{
     int i = width - str.length(); //width subtracted by string length 
@@ -90,14 +91,14 @@ std::string RJust(const std::string &str, int width, char fill) noexcept{
     return std::string(rightside, fill) + str;
 }
 
-std::string Replace(const std::string &str, const std::string &old, const std::string &rep) noexcept{
-    std::string finalString; //create a final variable
-    for (int i = 0; i < str.length(); ) { //setup loop to search through 'str'
-        if (str.compare(i, old.length(), old) == 0) { //use compare to look through string for the old substring
-            finalString += rep; //if the old substring is found, add the replacement substring to final variable
-            i += old.length(); //iterate the length of old substring to 'i' to skip the old substring
+std::string Replace(const std::string &str, const std::string &old, const std::string &rep) noexcept {
+    std::string finalString; // create a final variable
+    for (size_t i = 0; i < str.length(); ) { // setup loop to search through 'str'
+        if (str.compare(i, old.length(), old) == 0) { // use compare to look through string for the old substring
+            finalString += rep; // if the old substring is found, add the replacement substring to final variable
+            i += old.length(); // iterate the length of old substring to 'i' to skip the old substring
         } else {
-            finalString += str; //if no old substring is found, add the current letter to the final variable
+            finalString += str[i]; // if no old substring is found, add the current letter to the final variable
             i++;
         }
     }
@@ -114,7 +115,6 @@ std::string Join(const std::string &str, const std::vector< std::string > &vect)
     for (size_t i = 1; i < vect.size(); ++i) {
         result += str + vect[i];  // Add separator followed by the current element
     }
-
     return result;
 }
 
